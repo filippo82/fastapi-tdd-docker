a060bae74bf4a1a9c989644234b9777c7be8902b
Filippo Broggini <>
2022-03-05 18:57:37 +0100

    Code quality: flake8, black, isort

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Union
+from typing import List, Union  # ,Optional
 
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
@@ -12,12 +12,14 @@ async def post(payload: SummaryPayloadSchema) -> int:
     await summary.save()
     return summary.id
 
-#async def get(id: int) -> Optional[dict]:
+
+# async def get(id: int) -> Optional[dict]:
 async def get(id: int) -> Union[dict, None]:
     summary = await TextSummary.filter(id=id).first().values()
     if summary:
         return summary
     return None
+
 
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
--- project/app/db.py
+++ project/app/db.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI
 from tortoise import Tortoise, run_async
 from tortoise.contrib.fastapi import register_tortoise
 
-
 log = logging.getLogger("uvicorn")
 
 
@@ -28,6 +27,7 @@ def init_db(app: FastAPI) -> None:
         generate_schemas=False,
         add_exception_handlers=True,
     )
+
 
 async def generate_schema() -> None:
     log.info("Initializing Tortoise...")
